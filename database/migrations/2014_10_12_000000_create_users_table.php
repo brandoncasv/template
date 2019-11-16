27ee95fed34cab3281a8dcadf2bb573bfdaaa810
Camilo Rodriguez <hellsythex@gmail.com>
2019-11-16 11:41:38 -0600

    Fixed create users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('created_by')->unsigned()->index()->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('restrict');
+            $table->unsignedBigInteger('updated_by')->unsigned()->index()->nullable();
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('restrict');
+            $table->unsignedBigInteger('deleted_by')->unsigned()->index()->nullable();
+            $table->foreign('deleted_by')->references('id')->on('users')->onDelete('restrict');
+            $table->string('deleted_reason')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->smallInteger('status')->default('15');
+
             $table->string('avatar')->nullable();
             $table->string('name');
             $table->string('email')->unique();
@@ -29,11 +39,6 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->smallInteger('status')->default('15');
-            $table->unsignedBigInteger('created_by')->unsigned()->index()->nullable();
-            $table->foreign('created_by')->references('id')->on('users')->onDelete('restrict');
-            $table->unsignedBigInteger('updated_by')->unsigned()->index()->nullable();
-            $table->foreign('updated_by')->references('id')->on('users')->onDelete('restrict');
             $table->text('description')->nullable();
             $table->string('time_zone')->nullable();
             $table->string('language')->nullable();
