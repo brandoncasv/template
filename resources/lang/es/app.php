ee26941d8d6fc23195cceca8a277f44f6b070322
Camilo Rodriguez Cruz <crodriguez@capmega.com>
2020-03-03 18:36:16 -0600

    Added app translate

---
+++ resources/lang/es/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'home'        => 'Home',
+    'pages'       => [
+        'about'        => 'Acerca de Nosotros',
+        'privacy'      => 'Acuerdo de privacidad',
+        'terms'        => 'Términos y condiciones',
+        'faq'          => 'Preguntas frecuentes',
+    ],
+];
