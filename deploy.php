9aa086e07da3ee492bd99a5cf6e59d0aab139ba5
Camilo Rodriguez Cruz <crodriguez@capmega.com>
2020-03-11 13:35:47 -0600

    Implement Deployer =)

--- deploy.php
+++ deploy.php
@@ -3,33 +3,84 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
+//Global config
+set('http_user', 'www-data');
+
+// set default stage
+set('default_stage', 'production');
+
+// set permisions
+set('writable_use_sudo', true);
+set('clear_use_sudo', true);
+set('cleanup_use_sudo', true);
+
 // Project name
-set('application', 'my_project');
+set('application', 'sdk_template');
 
 // Project repository
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@github.com:sdkconsultoria/template.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', true);
 
-// Shared files/dirs between deploys 
+// Shared files/dirs between deploys
 add('shared_files', []);
 add('shared_dirs', []);
 
-// Writable dirs by web server 
+// Writable dirs by web server
 add('writable_dirs', []);
 
-
 // Hosts
+host('sdkconsultoria.com')
+    ->stage('production')
+    // ->set('user', 'www-data')
+    ->port(22)
+    ->set('deploy_path', '/var/www/html/{{application}}');
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
-    
+host('trial.sdkconsultoria.com')
+    ->stage('staging')
+    // ->set('user', 'www-data')
+    ->port(22)
+    ->set('deploy_path', '/var/www/html/{{application}}');
+
 // Tasks
+
+//Generate optimize bundles
+task('fix:npm_build', function () {
+    run('npm run build:back');
+    run('npm run build:front');
+})->local();
+
+//Send optimize functions
+task('fix:npm_send', function () {
+    upload(__DIR__ . "/public/bundle-back.js", '{{release_path}}/public/bundle-back.js');
+    upload(__DIR__ . "/public/bundle-front.js", '{{release_path}}/public/bundle-front.js');
+});
+
+//Fixed user-own
+task('fix:permisions', function () {
+    run('sudo chown www-data:www-data {{release_path}} -R');
+});
+
+//Group Fix
+task('fix', [
+    'fix:npm_build',
+    'fix:npm_send',
+    'fix:permisions',
+]);
 
 task('build', function () {
     run('cd {{release_path}} && build');
 });
+
+//back to previus release
+// task('npm', function () {
+//     if (has('previous_release')) {
+//         run('cp -R {{previous_release}}/node_modules {{release_path}}/node_modules');
+//     }
+//
+//     run('cd {{release_path}} && npm install');
+// });
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
@@ -37,4 +88,3 @@ after('deploy:failed', 'deploy:unlock');
 // Migrate database before symlink new release.
 
 before('deploy:symlink', 'artisan:migrate');
-
