d373e78fe17b8f3a338e08f97cb0ffe00b3c2db3
Camilo Rodriguez Cruz <crodriguez@capmega.com>
2020-03-11 17:15:49 -0600

    Clean deploy script

--- deploy.php
+++ deploy.php
@@ -18,16 +18,3 @@ host('sdkconsultoria.com')
     // ->set('user', 'www-data')
     ->port(22)
     ->set('deploy_path', '/var/www/html/{{application}}');
-
-host('trial.sdkconsultoria.com')
-    ->stage('staging')
-    // ->set('user', 'www-data')
-    ->port(22)
-    ->set('deploy_path', '/var/www/html/{{application}}');
-
-// Shared files/dirs between deploys
-add('shared_files', []);
-add('shared_dirs', []);
-
-// Writable dirs by web server
-add('writable_dirs', []);
